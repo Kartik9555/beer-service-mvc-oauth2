6048d037fd5e60c4a3ce3331eba0636b44ebd5f7
kartikeshwar.kandpal <kartikeshwar.kandpal@free-now.com>
2024-04-02 16:58:47 +0200

    Added OpenAPI support

--- src/main/java/com/example/practice/beerservicemvc/config/SecurityConfig.java
+++ src/main/java/com/example/practice/beerservicemvc/config/SecurityConfig.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -13,7 +13,16 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         /* Configuration for OAuth 2 Http Authentication */
-        http.authorizeHttpRequests(request -> request.anyRequest().authenticated());
+        http.csrf(AbstractHttpConfigurer::disable);
+        http.authorizeHttpRequests(request -> {
+            request.requestMatchers(
+                "/swagger-ui/**",
+                "/swagger-ui.html",
+                "/v3/api-docs**",
+                "/favicon.ico"
+            ).permitAll();
+            request.anyRequest().authenticated();
+        });
         http.oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()));
         return http.build();
     }
